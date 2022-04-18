b1b8eeb45bf5cbd878dc01a8c0b8755f022b7617
yorozco <yorozco@debian-BULLSEYE-live-builder-AMD64>
2022-04-18 16:46:28 -0400

    agregando funcionalidades

--- 01.js
+++ 01.js
@@ -6,6 +6,25 @@ const app = new Vue({
             {nombre:'Pera', cantidad:10},
             {nombre:'Manzana', cantidad:0},
             {nombre:'Platano', cantidad:11},    
-        ]
+        ],
+        nuevaFruta:'',
+        total: 0
+    },
+    methods :{
+        agregarFruta (){
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad:0
+            });
+            this.nuevaFruta = '';
+        }
+    },
+    computed:{
+        sumarFrutas(){
+            this.total = 0;
+            for(fruta of this.frutas){
+                this.total = this.total + fruta.cantidad;
+            }
+            return this.total;
+        }
     }
 })
