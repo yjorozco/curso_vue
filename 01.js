ea6af124bf17853b3afd7bf386d03ab8ee3bb1e9
yorozco <yorozco@debian-BULLSEYE-live-builder-AMD64>
2022-04-18 15:44:10 -0400

    first commit

---
+++ 01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola mundo con vue',
+        frutas:[
+            {nombre:'Pera', cantidad:10},
+            {nombre:'Manzana', cantidad:0},
+            {nombre:'Platano', cantidad:11},    
+        ]
+    }
+})
